165a0200d1ff834a5258aefd31e7d09c3a3bf63d
Sappasiri Nilnoppakhun <sappasiri.n@ku.th>
2022-11-09 01:52:33 +0700

    ส่งงาน

---
+++ server/routes/dormRoom.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+// controllers
+const { createDormRoom, listDormRoom, listDormRoomID, editDormRoomID , deleteDormRoomID } = require('../controllers/dormRoom')
+ 
+// middleware
+const { auth, adminCheck } = require('../middleware/auth')
+
+//Endpoint http://localhost:5000/api/dormroom
+router.post('/dormroom', auth, adminCheck, createDormRoom)
+router.get('/dormroom/:id',auth, listDormRoom)
+router.get('/dormroom/detail/:id',auth, listDormRoomID)
+router.put('/dormroom/edit/:id',auth, adminCheck, editDormRoomID)
+router.delete('/dormroom/delete/:id',auth, adminCheck, deleteDormRoomID)
+
+module.exports = router
---
+++ server/routes/submit.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// controllers
+const { createSubmit, listSubmit } = require('../controllers/submit')
+ 
+// middleware
+const { auth, adminCheck } = require('../middleware/auth')
+
+//Endpoint http://localhost:5000/api/submit
+router.post('/submit', createSubmit)
+router.get('/submit/:id', listSubmit)
+
+module.exports = router
