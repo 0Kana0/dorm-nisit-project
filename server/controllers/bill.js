6915e1ea6957d06ee7e79c1290447fd3bb4c2c0e
WinRecker <winnie2514@gmail.com>
2022-11-11 05:52:38 +0700

    update bill logic

--- server/controllers/bill.js
+++ server/controllers/bill.js
@@ -1,4 +1,7 @@
 const Bill = require('../models/Bill')
+const BillPay = require('../models/BillPay')
+const Submit = require('../models/Submit')
+const { DateTime } = require('luxon')
 
 exports.listBills = async (req,res) => {
   try {
@@ -6,6 +9,45 @@ exports.listBills = async (req,res) => {
     const bills = await Bill.find({}).populate('room').exec()
     console.log(bills)
     res.send(bills)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Server Error!')
+  }
+}
+
+exports.listTenantDormRoom = async (req,res) => {
+  try {
+    const { roomId } = req.params
+    const tenants = await Submit.find({dormroom:roomId}).populate('dormroom').populate('user').exec()
+    const pay = await BillPay.find({room:roomId}).exec()
+    const bill = await Bill.findOne({room:roomId}).exec()
+    const mappedTenants = tenants.map((item,index)=>{
+      if (bill !== null)
+        item = {...bill.toObject(),...item.toObject()}
+      let fine = 0
+      let issueDate = DateTime.fromJSDate(item.issueDate)
+      console.log(issueDate)
+      let dueDate = DateTime.fromJSDate(item.issueDate).plus({months:1})
+      let currentDate = DateTime.now()
+      if (currentDate > dueDate) {
+        let diff = currentDate.diff(dueDate,['days'])
+        let days = diff.as('days')
+        console.log('kk')
+        fine += Math.floor(days) * 20
+      }
+      item.dueDate = dueDate.toUTC().toISO()
+      const found = pay.find((bill)=>bill.user === item.user._id)
+      console.log(pay)
+      if (found) {
+        item.paid = true
+        item.fine = 0
+      } else {
+        item.paid = false
+        item.fine = fine
+      }
+      return item
+    })
+    res.send(mappedTenants)
   } catch (err) {
     console.log(err)
     res.status(500).send('Server Error!')
