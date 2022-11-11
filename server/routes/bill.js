0c9370b3da9b84b58fb0cd3359041e746de11636
WinRecker <winnie2514@gmail.com>
2022-11-11 07:26:31 +0700

    change server route URL

--- server/routes/bill.js
+++ server/routes/bill.js
@@ -11,7 +11,7 @@ const { auth, adminCheck } = require('../middleware/auth')
 // Endpoint
 router.get('/bill', auth, listBills)
 router.get('/bill/:dormId/:roomId',auth, adminCheck, listBillDormRoom)
-router.get('/bill/tenants/:dormId/:roomId',auth, adminCheck, listTenantDormRoom)
+router.get('/bill/:dormId/:roomId/:billId',auth, adminCheck, listTenantDormRoom)
 
 module.exports = router
 
