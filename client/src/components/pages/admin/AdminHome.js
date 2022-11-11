5c300f3225e81b7d1f3290f1090c372deee31292
WinRecker <winnie2514@gmail.com>
2022-11-11 08:26:57 +0700

    update admin home page

--- client/src/components/pages/admin/AdminHome.js
+++ client/src/components/pages/admin/AdminHome.js
@@ -1,14 +1,112 @@
-import React from 'react'
+import React,{useState} from 'react'
 import NavbarAdmin from '../../layouts/NavbarAdmin'
 import { useSelector } from 'react-redux'
+import Calendar from 'react-calendar';
+
+
 
 const AdminHome = () => {
   const {user} = useSelector((state)=>({...state}))
+  const [value, onChange] = useState(new Date());
+ 
   return (
     <div>
-      <h1>Hello {user.username}</h1>
-      <div>แอบรอ pull อยู่นะจ๊ะ แต่เธอไม่ push บ้างเลย</div>
+      
+      <div>
+        
+        <NavbarAdmin/>
+        <h1>Hello {user.username}</h1>
+      </div>
+      <div>
+        <div className='hotel'>
+      <img className='im' src="https://www.travelwanderlust.co/wp-content/uploads/2020/02/best-time-to-book-a-hotel.jpg" alt="Girl in a jacket" />
+      
     </div>
+    
+    <div className='Calendar'>
+      <Calendar onChange={onChange} value={value} />
+    </div>
+      <div class="container">
+  {/* <h2>ประกาศสำคัญ</h2> */}
+            
+  <table class=" ">
+    <thead>
+      <tr class='success'>
+        <th color='red'>ประกาศสำคัญ</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="success">
+        <td>1. แนวปฏิบัติการพักอาศัยหอพักนิสิตวิทยาเขตศรีราชา ประจำภาคปลาย ปี 2565</td>
+        
+      </tr>
+      <tr>
+        <td>2. ระบบจองห้องพักนิสิต ภาคปลาย ปี 2565</td>
+        
+      </tr>
+      <tr>
+        <td>3. ตารางการจดมิเตอร์น้ำประปา ไฟฟ้า และกำหนดชำระเงินภาคต้น</td>
+        
+      </tr>
+      <tr>
+        <td>4. ขอคืนค่าประกันของเสียหาย หอพักนิสิตมหาวิทยาลัยเกษตรศาสตร์ วิทยาเขต ศรีราชา</td>
+        
+      </tr>
+      <tr>
+        <td>5. การขอคืนเงินค่าธรรมเนียมหอพักนิสิตมหาวิทยาลัยเกษตรศาสตร์ วิทยาเขต ศรีราชา</td>
+        
+      </tr>
+      <tr>
+        <td>6. มาตรการป้องกันการแพร่ระบาดโรคติดต่อเชื้อไวรัสโคโรน่า 2019 นิสิตหอพักประจำภาคต้น 2565</td>
+        
+      </tr>
+      <tr>
+        <td>7. แนวปฏิบัติการพักอาศัยหอพักนิสิตวิทยาเขตศรีราชา ประจำภาคฤดูร้อนปี 2565</td>
+        
+      </tr>
+      <tr>
+        <td>8. ประชาสัมพันธ์หอ1-8 (ภาคปลาย 64)</td>
+        
+      </tr>
+      <tr>
+        <td>9. กำหนดวันจองหอพักนิสิต ปี 1 รหัส 65 (สำหรับ TCAS รอบ 2)</td>
+        
+      </tr>
+      <tr>
+        <td>10. แนวปฏิบัติการพักอาศัยหอพักนิสิตวิทยาเขตศรีราชา ประจำภาคต้นปีการศึกษา 2565</td>
+        
+      </tr>
+      <tr>
+        <td>11. แนวปฏิบัติการพักอาศัยหอพักนิสิตวิทยาเขตศรีราชา ประจำภาคปลาย ปีการศีกษา 2564</td>
+        
+      </tr>
+      <tr>
+        <td>12. ขยายเวลาและสิทธิ์การยื่นขอเงินค่าประกันของเสียหายประจำภาคต้นปี 64</td>
+        
+      </tr>
+      <tr>
+        <td>13. แนวทางการช่วยเหลือนิสิตหอพักมหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตศรีราชาที่ได้รับผลกระทบจากสถานการณ์ไวรัสโคโรนา 2019 ฉบับที่3</td>
+        
+      </tr>
+      <tr>
+        <td>14. มาตรการรองรับการเข้าพักของนิสิตหอพัก ภาคต้น ปีการศึกษา 2564</td>
+        
+      </tr>
+      <tr>
+        <td>15. กำหนดยื่นคำร้องขอเงินคืนค่าประกันของเสียหายและเงินตามแนวช่วยเหลือนิสิต COVID-19</td>
+        
+      </tr>
+      <tr>
+        <td>16. แนวปฏิบัติการพักอาศัยหอพักนิสิตวิทยาเขตศรีราชา ประจำภาคต้น ปีการศีกษา 2564</td>
+        
+      </tr>
+    </tbody>
+  </table>
+</div>
+      </div>
+    </div>
+
   )
 }
 
