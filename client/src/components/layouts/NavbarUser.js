f4e3448af93b71204e830f0cfb8d265af9608b74
WinRecker <winnie2514@gmail.com>
2022-11-11 07:37:54 +0700

    import Outlet

--- client/src/components/layouts/NavbarUser.js
+++ client/src/components/layouts/NavbarUser.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const NavbarUser = () => {
   const navigate = useNavigate()
@@ -14,34 +14,37 @@ const NavbarUser = () => {
     navigate('/')
   }
   return (
-    <nav className="navbar col-md-12 navbar-expand-sm navbar-dark bg-success sticky-top">
-      <div className="col-md-1"></div>
-      <div className="container-fluid">
-        <Link to="/user/index" className="navbar-brand">ระบบจองหอพัก</Link>
-        <div className="collapse navbar-collapse" id="navbarToggle">
+    <>
+      <nav className="navbar col-md-12 navbar-expand-sm navbar-dark bg-success sticky-top">
+        <div className="col-md-1"></div>
+        <div className="container-fluid">
+          <Link to="/user/index" className="navbar-brand">ระบบจองหอพัก</Link>
+          <div className="collapse navbar-collapse" id="navbarToggle">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to='/user/dormlist'>จองห้องพัก</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/user/UserBill'>ค่าไฟฟ้า</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/user/UserMainTain'>เเจ้งซ่อม</Link>
+              </li>
+            </ul>
+          </div>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to='/user/dormlist'>จองห้องพัก</Link>
+              <Link className="nav-link me-3" to="/user/dashboard"><i className="fa-solid fa-user"></i></Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to='/user/UserBill'>ค่าไฟฟ้า</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/user/UserMainTain'>เเจ้งซ่อม</Link>
+              <a className="nav-link me-3" onClick={logout}><i className="fa-solid fa-right-from-bracket"></i></a>
             </li>
           </ul>
         </div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link me-3" to="/user/dashboard"><i className="fa-solid fa-user"></i></Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link me-3" onClick={logout}><i className="fa-solid fa-right-from-bracket"></i></a>
-          </li>
-        </ul>
-      </div>
-      <div className="col-md-1"></div>
-    </nav>
+        <div className="col-md-1"></div>
+      </nav>
+      <Outlet/>
+    </>
   )
 }
 
